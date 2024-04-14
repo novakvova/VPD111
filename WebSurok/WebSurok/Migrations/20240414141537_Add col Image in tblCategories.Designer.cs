d683f992be5c23770c88a769f0e416c92705ec70
novakvova <novakvova@gmail.com>
2024-04-14 17:31:21 +0300

    Add image category

--- WebSurok/WebSurok/Migrations/20240414141537_Add col Image in tblCategories.Designer.cs
+++ WebSurok/WebSurok/Migrations/20240414141537_Add col Image in tblCategories.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSurok.Data;
 
@@ -10,9 +11,11 @@ using WebSurok.Data;
 namespace WebSurok.Migrations
 {
     [DbContext(typeof(MyAppContext))]
-    partial class MyAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240414141537_Add col Image in tblCategories")]
+    partial class AddcolImageintblCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
