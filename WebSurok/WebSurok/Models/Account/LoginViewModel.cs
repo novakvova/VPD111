9a850d2cbd9f32adb20db7c5f163dd45e6766dbe
novakvova <novakvova@gmail.com>
2024-04-28 15:40:11 +0300

    Update CRUD Auth User

--- WebSurok/WebSurok/Models/Account/LoginViewModel.cs
+++ WebSurok/WebSurok/Models/Account/LoginViewModel.cs
@@ -2,7 +2,15 @@
 {
     public class LoginViewModel
     {
+        /// <summary>
+        /// Логін користувача
+        /// </summary>
+        /// <example>admin@gmail.com</example>
         public string Email { get; set; }
+        /// <summary>
+        /// Логін користувача
+        /// </summary>
+        /// <example>123456</example>
         public string Password { get; set; }
     }
 }
--- WebSurok/WebSurok/Models/Categories/CategoryItemViewModel.cs
+++ WebSurok/WebSurok/Models/Categories/CategoryItemViewModel.cs
@@ -1,10 +1,10 @@
 ﻿namespace WebSurok.Models.Categories
 {
-    public class CategoryEditViewModel
+    public class CategoryItemViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public IFormFile Image { get; set; }
+        public string Image { get; set; }
     }
 }
